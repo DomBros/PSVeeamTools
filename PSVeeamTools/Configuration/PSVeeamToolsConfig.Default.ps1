d5f9334d8368887725f59bb2749cefa752169dd6
Tomasz Dąbrowski <dombros@gmail.com>
2023-04-20 15:16:05 +0200

    new PSVeeamTools version

--- PSVeeamTools/Configuration/PSVeeamToolsConfig.Default.ps1
+++ PSVeeamTools/Configuration/PSVeeamToolsConfig.Default.ps1
@@ -1,51 +1,83 @@
 # Default configuration object. It is used through whole PSVeeamTools module.
 
 $Script:VTConfig = [PSCustomObject]@{
-    VBRServer                             = 'VBR1'
+    VBRServer                             = 'VBR'
     LogFile                               = '{0}\PSVeeamTools-{1}.log' -f $env:TEMP, (Get-Date -Format 'yyyyMMddHHmmssfff')
     LogFileError                          = '{0}\PSVeeamToolsErrors-{1}.log' -f $env:TEMP, (Get-Date -Format 'yyyyMMddHHmmssfff')
     MailTo                                = 'IT-Alert-VeeamBackup@test.co.uk'
+    MailServer                            = 'mail.test.co.uk'
+    Domain                                = 'test.co.uk'
 
     ExcludeHostFromBackupSearch           = 'PLT*' # T stands for Test
     ExcludeBackupJob                      = 'Test'
-    ExcludeVMFromSureJob                  = 'WROVC', 'WRONTC', 'wrc-wfv'
+    ExcludeVMFromSureJob                  = 'WROVC', 'WRONTC', 'wrc-wf'
 
     DependencyToRestore                   = @(
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-A'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-A'
+            DuplicatedOnHost = $false
+            Host             = 'WHV6'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-B'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-B'
+            DuplicatedOnHost = $false
+            Host             = 'THV6'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-C'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-C'
+            DuplicatedOnHost = $false
+            Host             = 'THV3'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-E'
+            ComputerName     = 'GSDC0'
+            vLabName         = 'vLab-D'
+            DuplicatedOnHost = $false
+            Host             = 'GHV1'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-F'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-E'
+            DuplicatedOnHost = $false
+            Host             = 'THV4'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-G'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-F'
+            DuplicatedOnHost = $true
+            Host             = 'THV3'
         },
         [PSCustomObject]@{
-            ComputerName = 'TEST-AD'
-            vLabName     = 'vLab-J'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-G'
+            DuplicatedOnHost = $true
+            Host             = 'THV4'
         },
         [PSCustomObject]@{
-            ComputerName = 'LAB'
-            vLabName     = 'vLab-J'
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-H'
+            DuplicatedOnHost = $true
+            Host             = 'THV4'
         },
         [PSCustomObject]@{
-            ComputerName = 'PDC0'
-            vLabName     = 'vLab-S'
+            ComputerName     = 'SDC1'
+            vLabName         = 'vLab-J'
+            DuplicatedOnHost = $false
+            Host             = 'THV6'
+        },
+        [PSCustomObject]@{
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-S'
+            DuplicatedOnHost = $false
+            Host             = 'WHV5'
+        },
+        [PSCustomObject]@{
+            ComputerName     = 'PDC0'
+            vLabName         = 'vLab-T'
+            DuplicatedOnHost = $true
+            Host             = 'THV4'
         }
     )
     vLabParameters                        = @(
@@ -119,4 +151,22 @@ $Script:VTConfig = [PSCustomObject]@{
     SearchFormVMinBackupNotOlderThanHours = 72
     VmNeededInApplicationGroup            = 'PDC0'
     $DefaultVMvLan                        = '3'
+    Credentials                           = @{
+        HyperV = [PSCustomObject]@{
+            UserName = 'Domain\HV'
+            Password = $null
+        }
+        VMWare = [PSCustomObject]@{
+            UserName = 'Domain\VMW'
+            Password = $null
+        }
+        AD     = [PSCustomObject]@{
+            UserName = 'Domain\AD'
+            Password = $null
+        }
+        VBR    = [PSCustomObject]@{
+            UserName = 'Domain\Veam'
+            Password = $null
+        }
+    }
 }
